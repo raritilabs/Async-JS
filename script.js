65ce67962bb6a29981fbb9f5c54825e161c391dc
juhaina1996 <juhaina.mt@rariti.io>
2022-08-16 12:39:55 +0530

    DW-88: exercise on async JS

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     displayCocktail(data)
   })
   .catch((error) => console.error("FETCH ERROR:", error));
+  function displayCocktail(data) {
+    const cocktail = data.drinks[0];
+    const cocktailDiv = document.getElementById("cocktail");
+    const cocktailName = cocktail.strDrink;
+    const heading = document.createElement("h1");
+    heading.innerHTML = cocktailName;
+    cocktailDiv.appendChild(heading);
+    const cocktailImg = document.createElement("img");
+  cocktailImg.src = cocktail.strDrinkThumb;
+  cocktailDiv.appendChild(cocktailImg);
+  document.body.style.backgroundImage = "url('" + cocktail.strDrinkThumb + "')";
+  const cocktailIngredients = document.createElement("ul");
+  cocktailDiv.appendChild(cocktailIngredients);  
+  
+  const getIngredients = Object.keys(cocktail)
+  .filter(function (index) {
+    return index.indexOf("strIngredient") == 0;
+  })
+    .reduce(function (Array, index) {
+        if (cocktail[index] != null) {
+          Array[index] = cocktail[index];
+        }
+      return Array;
+    }, {});
+
+    for (let key in getIngredients) {
+        let value = getIngredients[key];
+        listItem = document.createElement("li");
+        listItem.innerHTML = value;
+        cocktailIngredients.appendChild(listItem);
+      }
+  }   
+  
